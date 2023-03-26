fdc2987c64252d47c6926e4189fb1b3e3a1c8a48
Joshua Grant <grantjoshua1995@gmail.com>
2023-03-26 15:59:35 +0200

    style text and images to fit better

---
+++ src/constants/legalStages.ts
@@ -0,0 +1,47 @@
+export const LEGAL_STAGES = [
+    {
+      id: 1,
+      stageName: "Battlefield",
+      img: "/images/stages/battlefield.webp",
+    },
+    {
+      id: 2,
+      stageName: "Final Destination",
+      img: "/images/stages/final-destination.jpg",
+    },
+    {
+      id: 3,
+      stageName: "Small Battlefield",
+      img: "/images/stages/small-battlefield.jpg",
+    },
+    {
+      id: 4,
+      stageName: "Town And City",
+      img: "/images/stages/town-and-city.webp",
+    },
+    {
+      id: 5,
+      stageName: "Kalos",
+      img: "/images/stages/kalos.webp",
+    },
+    {
+      id: 6,
+      stageName: "Hollow Bastion",
+      img: "/images/stages/hollow-bastion.jpg",
+    },
+    {
+      id: 7,
+      stageName: "Pokemon Stadium Two",
+      img: "/images/stages/pokemon-stadium-2.webp",
+    },
+    {
+      id: 8,
+      stageName: "Smashville",
+      img: "/images/stages/smashville.webp",
+    },
+    {
+      id: 9,
+      stageName: "Yoshis Story",
+      img: "/images/stages/yoshis-story.webp",
+    },
+  ];
