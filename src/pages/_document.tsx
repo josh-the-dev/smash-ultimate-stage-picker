b176093eb20e1761f4994e6562dec4f12ee0eb53
Joshua Grant <grantjoshua1995@gmail.com>
2023-03-30 23:54:49 +0200

    drop logo from bottom right

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,18 +6,10 @@ export default function Document() {
       <Head />
       <body>
         <Main />
-        <div className="absolute bottom-0 left-0">
+        <div className="absolute bottom-0 right-0">
           <h3 className="font-eras text-sm text-white italic">
             Developed by Amrux
           </h3>
-        </div>
-        <div className="absolute bottom-0 right-0">
-          <Image
-            src={"/images/logos/birthday-brawl-text.png"}
-            height={150}
-            width={150}
-            alt="logo"
-          />
         </div>
         <NextScript />
       </body>
