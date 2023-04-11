296246cfab8ad63a20175b132ea6fc68c3ec57cb
Joshua Grant <grantjoshua1995@gmail.com>
2023-04-11 23:35:52 +0200

    style players page with image and text

--- src/pages/players/index.tsx
+++ src/pages/players/index.tsx
@@ -1,6 +1,8 @@
 import CommonLayout from "@/layouts/CommonLayout";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
+import EnterTagImage from "../../../public/images/logos/enter-tag.png";
 
 const Players = () => {
   const [playerOne, setPlayerOne] = useState("");
@@ -19,23 +21,32 @@ const Players = () => {
 
   return (
     <CommonLayout>
+      <Image src={EnterTagImage} alt="Enter tag logo" />
+      <p className="text-black text-2xl font-eras font-bold mx-8 mb-8">
+        This updates your name on the quad stream and automates the VOD process,
+        so please enter your correct tag! :)
+      </p>
       <form onSubmit={handleSubmit} className=" flex flex-col gap-4">
-        <label htmlFor="PlayerOne">Player One</label>
+        <label htmlFor="PlayerOne" className="font-eras font-bold text-2xl">
+          Player 1
+        </label>
         <input
-          className="text-black"
+          className="text-black rounded-sm border-2 border-black"
           id="PlayerOne"
           value={playerOne}
           onChange={(e) => setPlayerOne(e.currentTarget.value)}
         />
-        <label htmlFor="PlayerTwo">Player Two</label>
+        <label htmlFor="PlayerTwo" className="font-eras font-bold text-2xl">
+          Player 2
+        </label>
         <input
-          className="text-black"
+          className="text-black rounded-sm border-2 border-black"
           id="PlayerTwo"
           value={playerTwo}
           onChange={(e) => setPlayerTwo(e.currentTarget.value)}
         />
-        <button className="bg-black p-4 rounder-md" type="submit">
-          Continue
+        <button className="bg-black p-4 rounder-md mt-4" type="submit">
+          Go
         </button>
       </form>
     </CommonLayout>
