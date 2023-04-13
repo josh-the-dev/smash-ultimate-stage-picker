7d26c9dc47658c2e5d2ba68c2f7752bb43c31adc
Joshua Grant <grantjoshua1995@gmail.com>
2023-04-13 17:50:04 +0200

    fix video bug when not on chrome

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,74 @@
 import CommonLayout from "@/layouts/CommonLayout";
 import { useRouter } from "next/router";
 import React, { useRef, useState } from "react";
+import { useUserAgent } from "next-useragent";
+import Image from "next/image";
+import BirthdayBrawlPic from "../../public/images/final_image_splash_screen.png";
 
-const Index = () => {
+//@ts-ignore
+export function getServerSideProps(context) {
+  return {
+    props: {
+      uaString: context.req.headers["user-agent"],
+    },
+  };
+}
+//@ts-ignore
+const Index = (props) => {
+  let ua;
+  if (props.uaString) {
+    ua = useUserAgent(props.uaString);
+  } else {
+    ua = useUserAgent(window.navigator.userAgent);
+  }
+  console.log(ua.browser);
   const [isVideoOver, setIsVideoOver] = useState(false);
   const router = useRouter();
+  if (ua.isChrome) {
+    return (
+      <CommonLayout>
+        <video
+          onEnded={() => setIsVideoOver(true)}
+          autoPlay
+          playsInline
+          className="min-w-full min-h-full max-h-full fixed right-0 bottom-0"
+          muted
+        >
+          <source src={"./videos/splash_screen.webm"} />
+        </video>
+
+        <div
+          className={`fixed bottom-8 w-full justify-center transition flex ease-in duration-300 ${
+            isVideoOver ? "opacity-100" : "opacity-0"
+          }`}
+        >
+          <button
+            onClick={() => {
+              if (router.query.id === "bb19e645-293e-4038-9783-c9ccdccfa6b7") {
+                router.push(`/picker?id=${router.query.id}`);
+              } else if (router.query.id) {
+                router.push(`/players?id=${router.query.id}`);
+              } else {
+                router.push(`/picker`);
+              }
+            }}
+            className="bg-black p-6 rounded-lg px-20"
+          >
+            Go
+          </button>
+        </div>
+      </CommonLayout>
+    );
+  }
   return (
     <CommonLayout>
-      <video
-        onEnded={() => setIsVideoOver(true)}
-        autoPlay
-        playsInline
+      <Image
+        src={BirthdayBrawlPic}
+        alt="Birthday Brawl Backdrop"
         className="min-w-full min-h-full max-h-full fixed right-0 bottom-0"
-        muted
-      >
-        <source src={"./videos/splash_screen.webm"} />
-      </video>
-
+      />
       <div
-        className={`fixed bottom-8 w-full justify-center transition flex ease-in duration-300 ${
-          isVideoOver ? "opacity-100" : "opacity-0"
-        }`}
+        className={`fixed bottom-8 w-full justify-center transition flex ease-in duration-300 `}
       >
         <button
           onClick={() => {
