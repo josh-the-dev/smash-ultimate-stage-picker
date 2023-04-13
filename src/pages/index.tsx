dec1a9c708c68e834fe60ae8fb2de3f201128129
Joshua Grant <grantjoshua1995@gmail.com>
2023-04-13 21:50:31 +0200

    fixing ids

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -67,11 +67,9 @@ const Index = (props) => {
       >
         <button
           onClick={() => {
-            if (router.query.id === "bb19e645-293e-4038-9783-c9ccdccfa6b7") {
+            if (router.query.id === "bb19e645-293e-4038-9283-c9ccdccfa6b7") {
               router.push(`/picker?id=${router.query.id}`);
-            } else if (
-              router.query.id !== "bb19e645-293e-4038-9783-c9ccdccfa6b7"
-            ) {
+            } else if (router.query.id) {
               router.push(`/players?id=${router.query.id}`);
             } else {
               router.push(`/picker`);
