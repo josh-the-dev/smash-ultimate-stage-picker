d21d9c6bc4c04f6057c971f7b806afb063ba6270
Joshua Grant <joshua.grant@tempcover.com>
2023-12-14 00:09:10 +0000

    start on logic for banning stages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import battlefieldImage from '$lib/assets/battlefield.webp';
+	let stageList = [
+		{ id: 1, name: 'Battlefield', isBanned: false, bannedBy: 0 },
+		{ id: 2, name: 'Final Destination', isBanned: false, bannedBy: 0 }
+	];
+
+	$: banCount = stageList.filter((s) => s.isBanned === true).length;
+	let gameCount = 0;
+
+	const gameBanMap = new Map([
+		[1, 8],
+		[2, 3],
+		[3, 3]
+	]);
+
+	$: currentMaxBans = gameBanMap.get(gameCount);
+	if (currentMaxBans && banCount >= currentMaxBans) {
+		// This is where we have our selected stage I guess.
+	}
+	const handleStageBan = (stageId: number) => {
+		const stageIndexToUpdate = stageList.findIndex((s) => s.id === stageId);
+		if (stageIndexToUpdate === -1) {
+			return;
+		}
+
+		stageList[stageIndexToUpdate].isBanned = true;
+	};
+</script>
+
+<h1>Welcome to smash stage selector</h1>
+
+<div class="flex gap-2">
+	{#each stageList as stage}
+		<p>{banCount}</p>
+		<button
+			class={`hover:opacity-40 ${stage.isBanned ? 'opacity-10' : null}`}
+			on:click={() => handleStageBan(stage.id)}
+		>
+			<img height="75" width="100" alt="Battlefield" src={battlefieldImage} />
+		</button>
+	{/each}
+</div>
