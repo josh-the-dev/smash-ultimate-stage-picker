e1abd55b3332d2321c01362a6bca33de27b1f8e0
Joshua Grant <grantjoshua1995@gmail.com>
2024-07-28 15:56:42 +0200

    add more content to centre. add font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,25 +6,37 @@
 	import FdLogo from '../lib/assets/stages/Icon_FD.png';
 	import Ps2Logo from '../lib/assets/stages/Icon_PS2.png';
 	import HBastion from '../lib/assets/stages/Icon_HBastion.png';
+	import Svile from '../lib/assets/stages/Icon_SVille.png';
+	import TnC from '../lib/assets/stages/Icon_TnC.png';
+	import Kalos from '../lib/assets/stages/Icon_Kalos.png';
+	import YStory from '../lib/assets/stages/Icon_YStory.png';
 </script>
 
 <div class="bg-[#1a1c1c] h-screen w-full">
 	<div class="mx-8 py-8">
 		<img width="400" src={LunacityLogo} alt="lunacity logo" />
-		<div class="bg-[#378169] pb-4 pt-1 px-2 rounded-lg">
-			<div class="flex justify-between my-2">
-				<h3>STAGE SELECTION</h3>
+		<div class="bg-[#378169] mt-4 pb-4 pt-1 px-3 rounded-lg">
+			<div class="flex items-center justify-between my-2 px-1">
+				<h3 class="font-pixelify text-3xl">STAGE SELECTION</h3>
 				<div class="flex gap-4">
-					<p>-</p>
-					<p>X</p>
+					<p class="font-pixelify text-3xl font-extrabold">-</p>
+					<p class="font-pixelify text-3xl font-extrabold">X</p>
 				</div>
 			</div>
-			<div class="bg-[#142c26] flex flex-col md:flex-row py-20 px-12 justify-between items-end">
-				<img src={BfLogo} class="h-20 w-24" alt="battlefield" />
-				<img src={SbfLogo} class="h-16 w-24" alt="small battlefield" />
-				<img src={FdLogo} class="h-20 w-24" alt="final destination" />
-				<img src={Ps2Logo} class="h-16 w-24" alt="pokemon stadium 2" />
-				<img src={HBastion} class="h-16 w-24" alt="hollow bastion" />
+			<div class="bg-[#142c26] py-20 px-12 flex flex-col gap-20">
+				<div class="flex justify-between items-end">
+					<img src={BfLogo} class="h-20 w-24" alt="battlefield" />
+					<img src={SbfLogo} class="h-16 w-24" alt="small battlefield" />
+					<img src={FdLogo} class="h-20 w-24" alt="final destination" />
+					<img src={Ps2Logo} class="h-16 w-24" alt="pokemon stadium 2" />
+					<img src={HBastion} class="h-16 w-24" alt="hollow bastion" />
+				</div>
+				<div class="flex justify-between px-28">
+					<img src={Svile} class="h-20 w-24" alt="smashville" />
+					<img src={TnC} class="h-20 w-24" alt="town and city" />
+					<img src={Kalos} class="h-20 w-24" alt="kalos" />
+					<img src={YStory} class="h-20 w-24" alt="yoshis story" />
+				</div>
 			</div>
 		</div>
 		<div class="my-12 mx-4 flex">
