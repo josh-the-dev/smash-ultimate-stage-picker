979279ea78ea3d01d33cf474b17bea1a5cd58703
Joshua Grant <grantjoshua1995@gmail.com>
2024-08-14 18:31:48 +0200

    fix cors issues

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(
+	cors({
+		origin: 'http://localhost:5173', // Include protocol and use proper URL format
+		methods: ['GET', 'POST'],
+		allowedHeaders: ['Content-Type', 'Authorization']
+	})
+);
 
 const http = require('http');
 const server = http.createServer(app);
