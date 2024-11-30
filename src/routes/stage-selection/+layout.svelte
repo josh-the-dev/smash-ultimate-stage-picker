0dd9534643eadfcdf6d0e1b34df06cf385184014
Joshua Grant <grantjoshua1995@gmail.com>
2024-11-30 12:23:29 +0100

    fix winner selection

--- src/routes/stage-selection/+layout.svelte
+++ src/routes/stage-selection/+layout.svelte
@@ -1,17 +1,32 @@
 <script lang="ts">
 	import '../../app.css';
 	import LunacityLogo from '$lib/assets/Lunacity_Text.png';
-
-	let streamCookie: string |null = null
 	// @ts-ignore
-	import * as cookies from 'cookie'
+	import * as cookie from 'cookie';
 
+	let showLogo = true;
 
+	// Parse cookies on component initialization
+	if (typeof document !== 'undefined') {
+		const cookies = cookie.parse(document.cookie);
+		const setupCookie = cookies['setup'];
+		
+		// Hide logo if setup cookie is set
+		if (setupCookie) {
+			showLogo = false;
+		}
+	}
 </script>
 
 <div class="p-20 py-8 max-h-screen">
 	<header class="flex justify-between items-center">
-		<img class="w-80 h-20" src={LunacityLogo} alt="lunacity logo" />
+		{#if showLogo}
+			<img 
+				class="w-80 h-20" 
+				src={LunacityLogo} 
+				alt="lunacity logo" 
+			/>
+		{/if}
 		<!-- <img class="h-14 mr-8" src={GentlemanButton} alt="Gentleman to stage" /> -->
 	</header>
 	<slot />
